253b6b3316840d5f83cec66966d6e82824ae833f
fernandovs4 <fervsantos3000@gmail.com>
2023-08-12 00:53:15 -0300

    teste

--- src/pages/Buscador.js
+++ src/pages/Buscador.js
@@ -67,6 +67,7 @@ const Buscador = () => {
                             toggleHospitalSelection={toggleHospitalSelection}
                         />
                         <ListRight
+                        
                             hospitaisSelecionados={hospitaisSelecionados}
                             toggleHospitalSelection={toggleHospitalSelection}
                         />
